26084d8d89b01ab1ee56a593e04bc4e5875ff060
yDavidw3 <davigames602@gmail.com>
2025-11-06 18:17:21 -0300

    Código de introdução ao JavaScipt

---
+++ prjAula12/JavaScriptIntroducao/js/script.js
@@ -0,0 +1,13 @@
+/*alert("Hello World!")
+console.log("Stay Hard!!")
+
+var numero1 = parseInt(prompt("Digite um número: "))
+var numero2 = parseInt(prompt("Digite um número: "))
+
+var soma = numero1+numero2
+var sub = numero1-numero2
+var multi = numero1*numero2
+var div = numero1/numero2
+
+alert("Total da soma: "+soma+", "+sub+", "+multi+", "+div)
+
